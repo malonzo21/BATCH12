692914c185eadb2ade61c7fd1c09f20e4464fdef
marbertalonzo <marbertalonzo@marberts-mbp.lan>
2024-09-14 10:41:26 -0400

    initial commit

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,18 +2,8 @@ package homework;
 
 public class GitHomework {
 
-	
-	
-	
 	public static void main(String[] args) {
 		System.out.println("BATCH 12: Git Class Lecture");
-		
 	}
-		
-		
-		public static void add(int a,int b) {
-			System.out.println(a+b);
-			
-		}
-	
+
 }
