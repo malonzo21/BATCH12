4de158db7445665104c5278d057417a428dd8746
marbertalonzo <marbertalonzo@marberts-mbp.lan>
2024-09-14 11:54:43 -0400

    removed code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,20 +2,5 @@ package homework;
 
 public class GitHomework {
 
-	public static void main(String[] args) {
-		System.out.println("BATCH 12: Git Class Lecture");
-		int age =12;
-		String name = "john";
-		//this is coming from github
-	}
-
 	
-	public static void add(int a,int b) {
-		System.out.println(a+b);
-	}
-	
-	
-	public static void divide(int m, int k) {
-		System.out.println(m/k);
-	}
 }
